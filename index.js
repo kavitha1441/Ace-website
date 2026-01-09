7d86151b3b7e3d5633b5e83a135ce21c40bb2b2f
kavitha1441 <kavithra.2509@gmail.com>
2026-01-09 11:24:58 +0530

    Fiirst commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var sidenav = document.querySelector(".side-nav-bar")
+
+function showNavbar()
+{
+    sidenav.style.display="block"
+}
+function closeNavbar()
+{
+    sidenav.style.display="none"
+}
